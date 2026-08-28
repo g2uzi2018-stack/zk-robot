36d1f4238294e5d11a2aa8d9cdf544d874f02eb1
gitadmin <zzkuang0516@163.com>
2026-08-27 23:01:07 -0600

    调整格式

--- src/common/logger.hpp
+++ src/common/logger.hpp
@@ -5,19 +5,19 @@
 namespace robot::common
 {
 
-// 获取项目统一 logger。
-//
-// 当前实现基于 spdlog 默认 logger。
-// 后续如果需要：
-// - 文件输出
-// - 日志轮转
-// - 不同 sink
-// - ROS2 logging
-//
-// 只需要修改这里，不影响业务代码。
-inline std::shared_ptr<spdlog::logger> logger()
-{
-    return spdlog::default_logger();
-}
+    // 获取项目统一 logger。
+    //
+    // 当前实现基于 spdlog 默认 logger。
+    // 后续如果需要：
+    // - 文件输出
+    // - 日志轮转
+    // - 不同 sink
+    // - ROS2 logging
+    //
+    // 只需要修改这里，不影响业务代码。
+    inline std::shared_ptr<spdlog::logger> logger()
+    {
+        return spdlog::default_logger();
+    }
 
 }
